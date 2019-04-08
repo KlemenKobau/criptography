75272de24ad259256346698d4c9601160ef49d77
Klemen Kobau <klemen.kobau@gmail.com>
2019-04-08 14:33:46 +0200

    started work on geffe generator

---
+++ geffe_generator.py
@@ -0,0 +1,14 @@
+from util import english_alphabet
+import binascii
+
+def letter_to_binary(letter):
+    # converts a letter to its binary representation
+    return str(bin(int.from_bytes(letter.encode(), 'big')))[4:]
+
+def letter_from_binary(binary):
+    binary_str = "0b"+binary
+    number = int(binary_str,2)
+    return english_alphabet[number - 1]
+
+if __name__ == "__main__":
+    print(letter_from_binary(letter_to_binary("G")))
