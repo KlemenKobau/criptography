79578c05d1f2c3face3132b558c06c9e5cea41c7
Klemen Kobau <klemen.kobau@gmail.com>
2019-05-23 21:29:25 +0200

    block finished

--- block_chain.py
+++ block_chain.py
@@ -1,6 +1,7 @@
 import hashlib
 import math
 import gmpy2
+import timeit
 
 #Code used to find collisions
 
@@ -73,20 +74,47 @@ def get_beta(p,alpha,a):
     print(beta)
     return beta
 
-def sign(thing_to_sign):
-    
+def get_gamma(alpha,k,p,q):
+    gamma = pow(alpha,k,p) % q
+    print(gamma)
+    return gamma
+
+def get_delta(k_inv,text_hash,a,gamma,q):
+    delta = k_inv * (text_hash + a * gamma) % q
+    print(delta)
+    return delta
+
+def get_line4_hash_pair(line1,line2,line3):
+    text_from_before =  line1 + "\n" + line2 + "\n" + line3
+    numero = 0
+    out = hashlib.sha1((text_from_before +"\n" +  str(numero)).encode('utf8')).hexdigest()
+    while(out[:7] != "0000000"):
+        numero += 1
+        out = hashlib.sha1((text_from_before +"\n"+ str(numero)).encode('utf8')).hexdigest()
+    print(numero,out)
+    return (numero,out)
+
 
 if __name__ == "__main__":
     text = "mining_5603404 mining_4772222"
 
+    # generating signature
     q = 1461501637330902918203684832716283019655932542983 # get_q()
     p = 262733146001979837311819117676584403741148754051683951627100716112228456309970554914202208782049869930601066827747097294307099992826430365197764768180688657032444227863596740797543810286283780951037400786218357893002160283961712113287405730217366139147492742038047382821740109907129666758685576696574382556186271021827359 #get_p(q)
 
     alpha = 117626421380962333891590668035460601041008165717093730108169129338965269816998200964546892525583313899216344748851557154156928253080707088151499561865862516727082982593945002962686926535155818673290476584572741790163139668897244037444014571248888150339750001163258228328999666293130311268832272046861181014271902656533152 # get_alpha(q,p)
     a = 61054 # random number form random.org
     beta = 128641743312107219780703511433184265271501813425778678809162113495573133202496183977620761529062190140181554662097772965664683979436070854641226878187129403732169565027611131637373437079581648598427075356057894352570575338713464141291037575483316999571400453886380035518944273849961741525063157573043381906527031082378095 # get_beta(p,alpha,a)
-
+    k = 534876 # another random number
+    gamma = 1190888160076453145260900606073456224541153317353 #get_gamma(alpha,k,p,q)
+    k_inv = pow(k,q-2,q) # can use this for inverse since q is prime https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
+    delta = 80979435450559784928743639696785877668909291907 #get_delta(k_inv,hash_coll(text),a,gamma,q)
     # public key is (p,q,alpha,beta)
 
+    # solution
     line1 = text + " " + str(653222382295586618370463671847145239723017395549) # the number is hashed collision
-    line2 = "klemenkobau " + 
+    line2 = "klemenkobau " + str(gamma) + " " + str(delta)
+    line3 = "000000049723160ba5b04dd37cc521ff3a98bfe1" # hash from ziga
+    #line4,line5 = get_line4_hash_pair(line1,line2,line3)
+    line4 = 324895060
+    line5 = "0000000b71c55fdc65377b1890a526b1d4c13580"
